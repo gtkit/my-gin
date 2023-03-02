0a9f3095adc2bf399a634b06bb64bf6bd64b5565
xiaozhaofu <xiaozhaofu@163.com>
2023-03-02 09:57:38 +0800

    fix:添加 lo测试

--- test/lo_test.go
+++ test/lo_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 
 	"github.com/samber/lo"
+
+	"ydsd_gin/tools/utils"
 )
 
 func TestStrLo(t *testing.T) {
@@ -17,4 +19,23 @@ func TestNumLo(t *testing.T) {
 	num := 0
 	ie := lo.IsEmpty(num)
 	t.Log("ie num----", ie)
+}
+
+func TestSliceLo(t *testing.T) {
+	a := make([]int, 0)
+	a = append(a, 1)
+	ie := lo.IsEmpty[int](len(a))
+	t.Log("ie slice---", ie)
+}
+
+func TestInterfaceLo(t *testing.T) {
+	var a interface{}
+	// a = make([]int, 0)
+	// a = make(map[string]string, 0)
+	t.Log("a is nil--", a == nil)
+	ie := lo.IsEmpty(a)
+	t.Log("ie interface---", ie)
+
+	ia := utils.Empty(a)
+	t.Log("ia interface----", ia)
 }
